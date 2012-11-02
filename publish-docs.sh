e02e2a823d924b5835a338586b4ea4ab4c07b546
Mikko Ohtamaa <mikko@opensourcehacker.com>
2012-11-02 20:08:24 +0200

    Removed jquery-interdependenices

--- publish-docs.sh
+++ publish-docs.sh
@@ -14,8 +14,9 @@ SOURCE=`pwd`
 TARGET=/tmp/docs-dist
 
 rm -rf $TARGET/gh-pages
-mkdir $TARGET/gh-pages
+install -d $TARGET/gh-pages
 
+echo "Entering $TARGET"
 cd $TARGET
 # Need to have gh-pages initialized first http://help.github.com/pages/
 git clone -b gh-pages git@github.com:miohtama/jquery-interdependencies.git gh-pages
@@ -23,9 +24,11 @@ git clone -b gh-pages git@github.com:miohtama/jquery-interdependencies.git gh-pa
 cd gh-pages
 # CSS issues: https://github.com/blog/572-bypassing-jekyll-on-github-pages
 touch .nojekyll
+# docs/ is on gitignore by default
 cp -r $SOURCE .
+rm .gitignore > /dev/null
 git add -A
 git commit -m "Updated gh-pages with new docs"
 git push origin gh-pages
-cd ..
-cd ..
+
+cd $SOURCE
